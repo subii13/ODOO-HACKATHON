d46fef1edb3d51e7eeb11e99773b92869a2487a4
Boomika <21boomika@gmail.com>
2026-07-12 11:30:50 +0530

    Implemented Firebase Authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import About from './pages/About';
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+import Login from "./pages/Auth/Login";
+import Signup from "./pages/Auth/Signup";
+
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 export default function App() {
   return (
     <>
       <Navbar brand="Team Name" />
+
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+  path="/about"
+  element={
+    <ProtectedRoute>
+      <About />
+    </ProtectedRoute>
+  }
+/>
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </>
   );
--- src/pages/Auth/Auth.jsx
+++
@@ -1,1 +0,0 @@
-// Role 3 - Auth screens
