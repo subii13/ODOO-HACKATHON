6873f8791488e423bed44ba5bc774f93f3bf3b93
vishwa <vishwapandimurugesan@gmail.com>
2026-07-12 15:09:46 +0530

    Completed vehicles and drivers module UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
 import { Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
+
 import Login from "./pages/Auth/Login";
 import Signup from "./pages/Auth/Signup";
+
 import ProtectedRoute from "./routes/ProtectedRoute";
+
 import MainFeature from "./pages/MainFeature/MainFeature";
 import FuelLogs from "./pages/FuelLogs/FuelLogs";
 import Maintenance from "./pages/Maintenance/Maintenance";
+
+import Vehicles from "./pages/MainFeature/Vehicles/Vehicles";
+import Drivers from "./pages/MainFeature/Drivers/Drivers";
 
 export default function App() {
   return (
     <>
       <Navbar brand="Team Name" />
+
       <Routes>
+        {/* Public Routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+
+        {/* Protected Routes */}
         <Route
           path="/"
           element={
@@ -24,6 +36,7 @@ export default function App() {
             </ProtectedRoute>
           }
         />
+
         <Route
           path="/about"
           element={
@@ -32,6 +45,7 @@ export default function App() {
             </ProtectedRoute>
           }
         />
+
         <Route
           path="/trips"
           element={
@@ -40,6 +54,25 @@ export default function App() {
             </ProtectedRoute>
           }
         />
+
+        <Route
+          path="/vehicles"
+          element={
+            <ProtectedRoute>
+              <Vehicles />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/drivers"
+          element={
+            <ProtectedRoute>
+              <Drivers />
+            </ProtectedRoute>
+          }
+        />
+
         <Route
           path="/fuel-logs"
           element={
@@ -48,6 +81,7 @@ export default function App() {
             </ProtectedRoute>
           }
         />
+
         <Route
           path="/maintenance"
           element={
